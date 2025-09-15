7f38b9818955412ab7d80d3aaac6e1af7876d647
karthikprathapaneni <192472038.simats@saveetha.com>
2025-09-15 08:43:50 +0530

    Create Experiment18.pl

---
+++ Experiment18.pl
@@ -0,0 +1,8 @@
+% --- Knowledge Base: people with their date of birth ---
+
+% Format: person(Name, dob(Day, Month, Year)).
+
+person(john,  dob(12, may, 1995)).
+person(mary,  dob(5,  july, 1998)).
+person(alice, dob(20, march, 1990)).
+person(bob,   dob(15, december, 2000)).
